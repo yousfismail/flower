5314b79b2125af05fd8a7cccb24eb86ccd63029d
yousfismail <yousfismail1200@gmail.com>
2023-05-01 08:07:08 +0300

    import (pages/login.dart) and (pushReplacement)

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,6 +1,6 @@
 import 'dart:developer';
 
-import 'package:flower_app/pages/login.dart';
+import 'package:flower_app/pages/login.dart'; 
 import 'package:flutter/material.dart';
 
 import '../shared/colors.dart';
