b5c938d596d2adbff6014d75848cd836d5359941
yousfismail <yousfismail1200@gmail.com>
2023-05-01 07:37:07 +0300

    Add keyboardType

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -22,9 +22,9 @@ class Login extends StatelessWidget {
               children:  [
                 const SizedBox(height: 64,),
                 
-                MyTextField (hinttexttt: 'Enter Your Email', isPassword: false, textInputTypeee: TextInputType.emailAddress,),
+                MyTextField  (hinttexttt: 'Enter Your Email', isPassword: false, textInputTypeee: TextInputType.emailAddress,),
                 const SizedBox(height: 33,),
-                MyTextField(hinttexttt: 'Enter Your Password', isPassword: true, textInputTypeee: TextInputType.text,),
+                MyTextField  (hinttexttt: 'Enter Your Password', isPassword: true, textInputTypeee: TextInputType.text,),
                 
                 const SizedBox(height: 33,), 
     
