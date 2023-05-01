5ec9e98846bb1c96072af4002b33933782a3327b
yousfismail <yousfismail1200@gmail.com>
2023-05-01 07:56:12 +0300

    Add new widget (children) in Row

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -40,7 +40,7 @@ class Login extends StatelessWidget {
     
                 const SizedBox(height: 33,), 
     
-                Row(
+                Row( 
                   mainAxisAlignment: MainAxisAlignment.center,
                    children: [
                     Text ('Do not have an account?' , style: TextStyle(fontSize: 18),),
