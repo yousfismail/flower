aba56ff52d89db87bc2748536a2c639eeabfe398
yousfismail <yousfismail1200@gmail.com>
2023-05-01 08:04:12 +0300

    Import ( pages/register.dart )

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:flower_app/pages/register.dart';
 
-import  'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -10,8 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,  
-      home: Home(), 
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
-  } 
+  }
 }
