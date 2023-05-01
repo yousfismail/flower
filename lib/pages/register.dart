f06cb097e2a01557eb3d51aa6f2e04db3c02bea6
yousfismail <yousfismail1200@gmail.com>
2023-05-01 08:05:04 +0300

    Code modification in the new (MyTextField)

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -23,12 +23,12 @@ class Register extends StatelessWidget {
                 children: [
                   const SizedBox(height: 64,),
             
-                  MyTextField(hinttexttt: 'Enter Your Username', isPassword: false, textInputTypeee: TextInputType.text,),
+                  MyTextField (hinttexttt: 'Enter Your Username', isPassword: false, textInputTypeee: TextInputType.text,),
                   const SizedBox(height: 33,),
                   
-                  MyTextField(hinttexttt: 'Enter Your Email', isPassword: false, textInputTypeee: TextInputType.emailAddress,),
+                  MyTextField (hinttexttt: 'Enter Your Email', isPassword: false, textInputTypeee: TextInputType.emailAddress,),
                   const SizedBox(height: 33,),
-                  MyTextField(hinttexttt: 'Enter Your Password', isPassword: true, textInputTypeee: TextInputType.text,),
+                  MyTextField (hinttexttt: 'Enter Your Password', isPassword: true, textInputTypeee: TextInputType.text,),
                   
                   const SizedBox(height: 33,),
                 
