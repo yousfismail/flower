1ddba53d7aa3712a5e6f442d5be5035c6203b2cf
yousfismail <yousfismail1200@gmail.com>
2023-05-01 07:44:07 +0300

    Create widget ( class MyTextField )

--- lib/constants/custom_textfield.dart
+++ lib/constants/custom_textfield.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+class MyTextField extends StatelessWidget {
+
+  final TextInputType textInputTypeee;
+  final bool isPassword;
+  final String hinttexttt;
+
+MyTextField({Key? key,
+  required this.textInputTypeee,
+  required this.isPassword,
+  required this.hinttexttt,
+}) : super (key: key);
+
+
+  @override
+  
+}
