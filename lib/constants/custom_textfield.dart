8588e41548fa6675cfb84f7faa784295830a1ed1
yousfismail <yousfismail1200@gmail.com>
2023-05-01 07:44:33 +0300

    Add const ( TextField )

--- lib/constants/custom_textfield.dart
+++ lib/constants/custom_textfield.dart
@@ -5,7 +5,7 @@ class MyTextField extends StatelessWidget {
   final bool isPassword;
   final String hinttexttt;
 
-MyTextField({Key? key,
+ MyTextField({Key? key,
   required this.textInputTypeee,
   required this.isPassword,
   required this.hinttexttt,
@@ -13,5 +13,17 @@ MyTextField({Key? key,
 
 
   @override
-  
+  Widget build(BuildContext context) {
+    return  TextField(
+                keyboardType: textInputTypeee,
+                obscureText: isPassword,
+                decoration: InputDecoration(
+                  hintText: hinttexttt,
+                  enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
+                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey,),),
+                  filled: true,
+                  contentPadding: const EdgeInsets.all(8),
+                  )
+              );
+  }
 }
